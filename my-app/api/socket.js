e9fc8db9cee1cbaa87350d67cc89687d9b9fae2c
same10b <armys10b@gmail.com>
2024-12-19 00:03:00 +0900

    Add files via upload

--- my-app/api/socket.js
+++ my-app/api/socket.js
@@ -1,22 +1,19 @@
-const { Server } = require('socket.io');
+const { Server } = require("socket.io");
 
 module.exports = (req, res) => {
-  // Expressのサーバーを作成してSocket.ioを設定
   const io = new Server(res.socket.server);
   io.on('connection', (socket) => {
-    console.log('User connected');
-    
-    // クライアントからのメッセージを受け取って全員に送信
+    console.log('a user connected');
     socket.on('inputText', (text) => {
-      console.log('Received text:', text);
-      io.emit('outputText', text);  // すべてのクライアントに送信
+      console.log('Accumulated Text:', text); // デバッグ用
+      io.emit('inputText', text);  // 出力ページに送信
     });
 
     socket.on('disconnect', () => {
-      console.log('User disconnected');
+      console.log('user disconnected');
     });
   });
 
-  // レスポンスを送信
-  res.send('Socket.io server is running');
+  res.socket.server.io = io;
+  res.status(200).send("Socket.IO server is running");
 };
